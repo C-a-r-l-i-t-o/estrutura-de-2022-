d3e533b825eb46f1e6c0cac296cbea622e5e6f1a
C-a-r-l-i-t-o <carlimtkd@hotmail.com>
2022-08-01 16:06:34 -0300

    atualizacao de estrutura de dados

---
+++ float_vector.c
@@ -0,0 +1,32 @@
+#include "float_vector.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct float_vector {
+    int capacidade; //numero max de elementos
+    int size;//quantidade atual de elementos
+    float *data;// vetor de floats
+
+};
+/**
+ * @brief Cria (aloca) um vetor de floats com uma data capacidade
+ * 
+ * @param capacity capacidade do vetor.
+ * @return FloatVector*  Um vetor de floats alocado/ criado.
+ */
+FloatVector *create(int tam)
+{
+    FloatVector *vet =(FloatVector *) calloc(1, sizeof(FloatVector));
+    vet->capacity = tam;
+    vet->size = 0;
+    vet->data =(float*) calloc(vet->capacity, sizeof(float));
+
+    return vet;
+}
+
+void destroy(FloatVector **vet_ref){
+    FloatVector *vet =*vet_ref;
+    free(vet->data);//
+    free(vet);
+    *vet_ref = NULL;
+}
