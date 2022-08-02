9e100bfc990cac3a248454d94f6f9eb33ee020ed
C-a-r-l-i-t-o <98427854+C-a-r-l-i-t-o@users.noreply.github.com>
2022-08-02 14:46:17 -0300

    Update float_vector.h

--- float_vector.h
+++ float_vector.h
@@ -6,7 +6,7 @@ void destroy (FloatVector **ref_vector );
 int size(const FloatVector *vector);
 int capacity(const FloatVector *vector);
 float at(const FloatVector *vector, int pos);
-float get(const FloatVector *vector);
+float get(const FloatVector *vector, int pos);
 void append(FloatVector *vector, float n);
 void print(const FloatVector *vector);
 
